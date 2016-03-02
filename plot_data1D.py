117f7df64b5a3569250467b9d2d89be8a4098ecf
Marco <none@none>
2016-03-03 00:38:49 +0100

    Switch from TH1 to THStack, new flags and colortable dded

--- plot_data1D.py
+++ plot_data1D.py
@@ -40,6 +40,9 @@ ROOT.gStyle.SetPadTopMargin(0.06)
 ROOT.gStyle.SetPadBottomMargin(0.1)
 ROOT.gStyle.SetTitleOffset(1.1,"y")
 
+markertable = {0:[ROOT.kAzure+2,33,1.8],1:[ROOT.kSpring-8,8,1.0],2:[ROOT.kRed+1,21,1.0],3:[ROOT.kOrange+1,34,1.2],4:[ROOT.kAzure+2,22,1.2],5:[ROOT.kSpring-8,23,1.2],6:[ROOT.kRed+1,29,1.6],7:[ROOT.kOrange+1,21,1.0]}
+
+
 fileparser = argparse.ArgumentParser()
 fileparser.add_argument("-f", "--filename")
 fileparser.add_argument("-nc", "--numbercolumns")
@@ -61,6 +64,9 @@ parser.add_argument("-sx", "--sizex", default=1200)
 parser.add_argument("-sy", "--sizey", default=900)
 parser.add_argument("-n", "--name", default="plot")
 parser.add_argument("-s", "--save", action="store_true")
+parser.add_argument("-ms", "--markersize", default=1.)
+parser.add_argument("-w", "--wait", action="store_true")
+parser.add_argument("-lp", "--legendposition", nargs =2, default=[0.5,0.8], type=float) #Defines top left corner of TLegend
 #parser.add_argument("-", "--")
 
 filelist = ()
@@ -102,63 +108,96 @@ ex = np.zeros(len(NBins))
 
 ########## Data point input
 
-FillValues = ()
-FillValues += Read_Data(filelist[0])
+#FillValues = np.zeros((len(filelist),len(BinEdges)-1), dtype=object)
+FillValues = []
 
-#print FillValues
+for i in range (len(filelist)):
+    FillValues.append(Read_Data(filelist[i]))
+
 
 ########## General histogram
 TC1 = ROOT.TCanvas("TC1","",20,20,config.sizex,config.sizey)
-TH1Plot = ROOT.TH1D("TH1Plot",config.title,len(NBins),np.array(BinEdges,'d'))
+if config.tgrapherrors:
+    TH1Plot = ROOT.TH1D("TH1Plot",config.title,len(NBins),np.array(BinEdges,'d'))
+else:
+    THSt1 = ROOT.THStack("THStack1",config.title)
+    TH1Plot = []
+
 
 if config.xlog:
     ROOT.gPad.SetLogx()
 if config.ylog:
     ROOT.gPad.SetLogy()
-for i in range(0,len(FillValues)):
-    if config.tgrapherrors:
-        TGE1 = ROOT.TGraphErrors(len(Bins),np.array(Bins,'d'),unumpy.nominal_values(FillValues),ex,unumpy.std_devs(FillValues))
-        TGE1.SetMarkerStyle(33)
-        TGE1.SetMarkerColor(ROOT.kAzure+2)
-        TGE1.SetMarkerSize(1.8)
-        if ( len(BinEdges)-1 != len(FillValues) ) :
-            sys.exit( "Size of binning and number of values don't agree. Stopping macro!")
-    else:
-        TH1Plot.SetContent(unumpy.nominal_values(FillValues))
-        TH1Plot.SetError(unumpy.std_devs(FillValues))
-        TH1Plot.SetMarkerColor(ROOT.kAzure+2)
-        TH1Plot.SetMarkerStyle(33)
-        TH1Plot.SetMarkerSize(1.8)
-        if ( len(BinEdges) != len(FillValues) ) :
-            sys.exit( "Size of binning and number of values don't agree. Stopping macro!")
 
-TLeg = ROOT.TLegend(0.5,0.5,0.8,0.45)
+
+TLeg = ROOT.TLegend(config.legendposition[0],config.legendposition[1],config.legendposition[0]+0.05,config.legendposition[1]-len(filelist)*0.04)
 TLeg.SetFillColor(0)
 TLeg.SetMargin(0.00)
 TLeg.SetBorderSize(0)
+TLeg.SetTextFont(font2use)
+TLeg.SetTextSize(fontsize)
 
-TLeg.AddEntry(TGE1, ("  %s"%(' '.join(filelegend[0]))))
+for i in range(0,len(FillValues)):
+    if config.tgrapherrors:
+        TGE1 = ROOT.TGraphErrors(len(Bins),np.array(Bins,'d'),unumpy.nominal_values(FillValues[i]),ex,unumpy.std_devs(FillValues[i]))
+        TGE1.SetMarkerColor(markertable.get(i)[0])
+        TGE1.SetMarkerStyle(markertable.get(i)[1])
+        TGE1.SetMarkerSize(markertable.get(i)[2]*config.markersize)
+        if ( len(BinEdges)-1 != len(FillValues[i]) ) :
+            sys.exit( "Size of binning and number of values don't agree. Stopping macro!")
+        TLeg.AddEntry(TGE1, ("  %s"%(' '.join(filelegend[i]))))
+    else:
+        TH1Plot.append(ROOT.TH1D("TH1Plot","%i"%(i),len(NBins),np.array(BinEdges,'d')))
+        TH1Plot[i].SetContent(unumpy.nominal_values(FillValues[i]))
+        TH1Plot[i].SetError(unumpy.std_devs(FillValues[i]))
+        TH1Plot[i].SetMarkerColor(markertable.get(i)[0])
+        TH1Plot[i].SetMarkerStyle(markertable.get(i)[1])
+        TH1Plot[i].SetMarkerSize(markertable.get(i)[2]*config.markersize)
+        if ( len(BinEdges) != len(FillValues[i]) ) :
+            sys.exit( "Size of binning and number of values don't agree. Stopping macro!")
+        THSt1.Add(TH1Plot[i])
+        TLeg.AddEntry(TH1Plot[i], ("  %s"%(' '.join(filelegend[i]))))
 
-TH1Plot.GetXaxis().SetTitle(config.xtitle)
-TH1Plot.GetYaxis().SetTitle(config.ytitle)
-if config.xrange:
-    TH1Plot.GetXaxis().SetRangeUser(config.xrange[0],config.xrange[1])
-if config.yrange:
-    TH1Plot.GetYaxis().SetRangeUser(config.yrange[0],config.yrange[1])
+
+if config.tgrapherrors:
+    TH1Plot.GetXaxis().SetTitle(config.xtitle)
+    TH1Plot.GetYaxis().SetTitle(config.ytitle)
+    if config.xrange:
+        TH1Plot.GetXaxis().SetRangeUser(config.xrange[0],config.xrange[1])
+    if config.yrange:
+        TH1Plot.GetYaxis().SetRangeUser(config.yrange[0],config.yrange[1])
+else:
+    THSt1.Draw("nostack")
+    THSt1.GetXaxis().SetTitle(config.xtitle)
+    THSt1.GetYaxis().SetTitle(config.ytitle)
+    if config.xrange:
+        THSt1.GetXaxis().SetRangeUser(config.xrange[0],config.xrange[1])
+        print "set x-range"
+    if config.yrange:
+        THSt1.SetMinimum(config.yrange[0])
+        THSt1.SetMaximum(config.yrange[1])
+        print "set y-range"
 
 
 ########## Plotting
 
-TH1Plot.DrawCopy()
 if config.tgrapherrors:
     try:
+        TH1Plot.DrawCopy()
         TGE1.Draw("sameP")
     except:
         pass
+else:
+    THSt1.Draw("nostack")
+
+#TLeg = TC1.BuildLegend()
+#TLeg.AddEntry("","test", "")
 TLeg.Draw("")
 
-#wait()
+if config.wait:
+    wait()
 
-TC1.SaveAs("%s.pdf"%(config.name))
-TC1.SaveAs("%s.png"%(config.name))
+if config.save:
+    TC1.SaveAs("plots/%s.pdf"%(config.name))
+    TC1.SaveAs("plots/%s.png"%(config.name))
 
